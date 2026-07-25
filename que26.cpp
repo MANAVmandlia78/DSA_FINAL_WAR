4f3e513f574876c28d24157db5fa7e4993f2e870
MANAVmandalia78 <manavmandalia077@gmail.com>
2026-07-25 20:08:57 +0530

    a

---
+++ que26.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isrotation(string s1,string s2){
+    if(s1.length() != s2.length()){
+        return false;
+    }
+
+    string temp = s1 + s1;
+
+    if(temp.find(s2) != string::npos){
+        return true;
+    }else{
+        return false;
+    }
+
+    return false;
+}
+
+int main(){
+    string s1 = "ABCD";
+    string s2 = "AAAA";
+
+    if(isrotation(s1,s2)){
+        cout<<"yes";
+    }else{
+        cout<<"No";
+    }
+}
