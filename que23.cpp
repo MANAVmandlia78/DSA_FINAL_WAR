6738e7b5e5694d843db1844331837dafc5351fb6
MANAVmandalia78 <manavmandalia077@gmail.com>
2026-07-25 17:59:46 +0530

    a

---
+++ que23.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs)
+{
+    if(strs.empty())
+        return "";
+
+    string ans = "";
+
+    for(int i = 0; i < strs[0].length(); i++)
+    {
+        char ch = strs[0][i];
+
+        for(int j = 1; j < strs.size(); j++)
+        {
+            if(i >= strs[j].length() || strs[j][i] != ch)
+                return ans;
+        }
+
+        ans += ch;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    vector<string> strs = {"flower","flow","flight"};
+
+    cout << longestCommonPrefix(strs);
+}
