fb83d00e4c0b12c1d2c9b10f06a6f04eb322fcbf
MANAVmandalia78 <manavmandalia077@gmail.com>
2026-07-28 14:31:14 +0530

    a

---
+++ que43.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int search(int arr[],int n,int target){
+    int low = 0;
+    int high = n -1;
+
+    while(low<=high){
+        int mid = (low+high)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+
+        if(arr[low]<=arr[mid]){
+            if(target >= arr[low] && target < arr[mid]){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+
+        else{
+            if(target > arr[mid] && target <= arr[high]){
+                low = mid+1;
+            }else{
+                high = mid -1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int arr[] = {4,5,6,7,1,2,3};
+    int n = 7;
+    int target = 2;
+
+    cout<< search(arr,n,target);
+}
