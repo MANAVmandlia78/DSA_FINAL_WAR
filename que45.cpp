e66cb6bf756028da3963d47179fa4512855a8410
MANAVmandalia78 <manavmandalia077@gmail.com>
2026-07-31 23:46:25 +0530

    a

---
+++ que45.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubblesort(int arr[],int n){
+    for(int i = 0 ; i < n - 1; i++){
+        bool swapped = false;
+        for(int j = 0; j < n - i -1 ;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                swapped = true;
+            }
+        }
+
+        if(swapped == false){
+            break;
+        }
+    }
+}
+
+int main(){
+    int arr[] = {5,1,4,2,8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bubblesort(arr,n);
+    for(int i = 0; i < n;i++){
+        cout << arr[i] << " ";
+    }
+}
